f51824745489db84d2024b4e817d38fca186689a
norhu1130 <norhu1130@naver.com>
2021-05-31 14:48:36 +0900

    arm64

--- app/assets/js/scripts/uicore.js
+++ app/assets/js/scripts/uicore.js
@@ -41,7 +41,7 @@ if(!isDev){
                 loggerAutoUpdaterSuccess.log('New update available', info.version)
                 
                 if(process.platform === 'darwin'){
-                    info.darwindownload = `https://github.com/norhu1130/Launcher/releases/download/v${info.version}/Mercurylauncher-setup-${info.version}.dmg`
+                    info.darwindownload = `https://github.com/norhu1130/Launcher/releases/download/v${info.version}/Mercurylauncher-setup-${info.version}${process.arch === 'arm64' ? '-arm64' : ''}.dmg`
                     showUpdateUI(info)
                 }
                 
